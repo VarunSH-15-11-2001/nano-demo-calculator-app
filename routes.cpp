16e83795a7655893fbb4e46a71decba78b692de9
Varun ShankarHoskere <varunhoskere@gmail.com>
2023-08-20 20:46:08 +0530

    cpp works?

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,27 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello World!"};
 }
-crow::response add(const crow::request &req)
+crow::response add(const crow::request& req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+
+    int firstValue = input["first"].i();
+    int secondValue = input["second"].i();
+
+    int result = firstValue + secondValue;
+
+    return crow::response { "{\"result\":" + to_string(result) + "}" };
 }
-crow::response subtract(const crow::request &req)
+crow::response add(const crow::request& req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+
+    int firstValue = input["first"].i();
+    int secondValue = input["second"].i();
+
+    int result = firstValue - secondValue;
+
+    return crow::response { "{\"result\":" + to_string(result) + "}" };
 }
