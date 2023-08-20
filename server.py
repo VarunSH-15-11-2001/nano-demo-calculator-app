21683d13ebe162657bd094e4710c284617464c3f
Varun ShankarHoskere <varunhoskere@gmail.com>
2023-08-20 10:03:44 +0530

    update 1

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, requests
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!', 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    n = requests['first']
+    m = requests['second']
+    k = n+m
+    return '%d'%k, 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    n = requests['first']
+    m = requests['second']
+    k = n-m
+    return '%d'%k, 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
